9de7a48ce24ef38a55d3a0f99430afba1f123a1c
MrWhip <gleb.kovalev.02@mail.ru>
2022-03-30 01:26:59 +0300

    Доделал лендинг (за исключением слайдера и адаптивов)

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var divs = document.querySelectorAll('whomBlock__button'), i = 0;
-   change.onclick = function() {
-        divs[i].style.display="none";
-        i = (i < (divs.length - 1))?i+1:0;
-        divs[i].style.display="block";
-   }
+const tabs = document.querySelectorAll('.whomBlock__tab')
+const buttons = document.querySelectorAll('.whomBlock__button')
+
+function select(idx) {
+     for (let tab of tabs)
+        tab.style.display = 'none'
+
+     tabs[idx].style.display = 'flex'
+
+     for (let button of buttons)
+        button.classList.remove('whomBlock__button_active')
+
+     buttons[idx].classList.add('whomBlock__button_active')
+}
+
+for (let i = 0; i < buttons.length; i++) {
+     buttons[i].addEventListener('click', select.bind(null, i))
+}
